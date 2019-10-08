3d977f8f40f3a26711cd53d41737badf8564f33a
Derek P <derektprice@gmail.com>
2019-10-08 11:55:21 -0500

    Update Scrape_and_Save_1.0.py
    Added more comments to make it clear what each part of the program does

--- Scrape_and_Save_1.0.py
+++ Scrape_and_Save_1.0.py
@@ -3,22 +3,25 @@ from bs4 import BeautifulSoup
 from time import sleep
 import datetime
 
-
+#this block creates a list of the urls you want to scrape, and counts how long that list is
 html_list = ["INSERT_URLS_HERE"]
 html_list_length = len(html_list)
 num_of_files = list(range(html_list_length))
 num_of_files.reverse()
 html_source_filename_list = []
 
+#defining headers for polite scraping
 hdr = {"User-Agent": "NAME",
            "From": "EMAIL_ADDRESS"}
 
+#creates a file name for each element in your url list
 def make_filename(website):
     d = datetime.datetime.now()
     file_name = f"{website} {num_of_files.pop()} - {d:%d-%B-%Y--%H.%M}.html"
     html_source_filename_list.append(file_name)
     return file_name
 
+#this actually requests the pages and runs the make_filename function
 def save_html_source(websites, website_name):
     for thing in websites:
         page = requests.get(thing, headers=hdr)
@@ -27,5 +30,6 @@ def save_html_source(websites, website_name):
         with open(make_filename(website_name), "wb") as file:
             file.write(soup.encode('utf-8-sig'))
             file.close()
-
-save_html_source(html_list, "HTML_FILENAME")
+                      
+#I'm bad with object-oriented programming so I just run one function to run the whole program - this is probably bad form but it works!
+save_html_source(html_list, "HTML_FILENAME_STEM")
